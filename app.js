42e9fce2026bb929e447b860a3ad028f525f8339
obamo1 <joobamo@gmail.com>
2021-12-08 16:36:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function(){
+$('.js-form').submit(e =>{
+e.preventDefault();
+  const userTextElement = $(event.currentTarget).find('#user-text');
+  $('.js-display-user-text').text(`Item: ${userTextElement.val()}`);
+  userTextElement.val("");
+})
+})
